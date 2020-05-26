fe47a1c28de0d0b37bedb831fbbd408e932060ae
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-26 22:42:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,22 @@
 require "pry"
+def join_nested_strings(src)
+ outer_result = []
+ row_index = 0 
+ while row_index < src.count do
+   element_index = 0 
+   while element_index < src[row_index].count do 
+   if src[row_index][element_index].class == String
+     outer_result << src[row_index][element_index]
+   end
+   element_index += 1 
+   end 
+  row_index += 1 
+end 
+outer_result.join(" ")
+end
 
 
-
+#second solution 
 
 def join_nested_strings(src)
   outer_result = []
